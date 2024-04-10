597b3c9f39a063e05eec3af921b986f958edd634
Your Name <you@example.com>
2024-04-10 20:07:11 -0300

    adding class 8 file

---
+++ 8-positivoNegativoOu0.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Digite um número para descobrir seu sinal: ");
+    scanf("%d", &n);
+
+    if (n > 0) {
+        printf("O número é positivo");
+        return 0;
+    }
+
+    if (n < 0) {
+        printf("O número é negativo");
+        return 0;
+    }
+
+    printf("O número é 0");
+
+    return 0;
+}
